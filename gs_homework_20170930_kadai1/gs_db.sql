0632f08bc17c4bf01f4e2a651cd18e36252020fa
Ryusuke <esm0yc1cse9rir@gmail.com>
2017-10-06 08:36:41 +0900

    no message

---
+++ gs_homework_20170930_kadai1/gs_db.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2017 年 10 月 06 日 01:34
+-- サーバのバージョン： 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE IF NOT EXISTS `gs_an_table` (
+`id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `naiyou` text COLLATE utf8_unicode_ci,
+  `indate` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `email`, `naiyou`, `indate`) VALUES
+(3, 'test100', 'test1', 'test1', '2017-09-30 16:34:20'),
+(5, 'test3', 'test3', 'test3', '2017-09-30 16:34:38'),
+(6, 'BBB', 'BBB', 'OOO', '2017-10-03 19:39:53'),
+(7, 'ooo', 'ppp', 'kkkzzz', '2017-10-04 21:52:02');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE IF NOT EXISTS `gs_bm_table` (
+`id` int(12) NOT NULL,
+  `book_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `book_url` text COLLATE utf8_unicode_ci NOT NULL,
+  `book_comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `indate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `book_name`, `book_url`, `book_comment`, `indate`) VALUES
+(2, '111000', '222', 'qqqq', '2017-09-30 14:22:44'),
+(3, 'test', 'test', 'tesst', '2017-10-04 23:01:44'),
+(4, 'test1', 'test', 'test', '2017-10-04 23:17:31'),
+(5, 'testA', 'test', 'test', '2017-10-04 23:21:43'),
+(6, 'testin', 'test', 'tesqqqq', '2017-10-05 20:57:19');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_user_table`
+--
+
+CREATE TABLE IF NOT EXISTS `gs_user_table` (
+`id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `lid` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `lpw` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `kanri_flg` int(1) NOT NULL,
+  `life_flg` int(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_user_table`
+--
+
+INSERT INTO `gs_user_table` (`id`, `name`, `lid`, `lpw`, `kanri_flg`, `life_flg`) VALUES
+(3, '123', 'test', '123test', 0, 0),
+(4, '321', '456', '123456', 1, 0),
+(5, '987', 'ooo', 'tyau', 0, 0),
+(6, 'Q', 'W', 'E', 0, 0),
+(7, 'RYu', 'test', '123test', 0, 0),
+(8, '123', 'test', '123test', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
