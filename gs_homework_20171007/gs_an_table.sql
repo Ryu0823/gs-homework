e91c596c45cb77e4f564e461ab0064d1842fafa4
Ryusuke <esm0yc1cse9rir@gmail.com>
2017-10-12 23:15:49 +0900

    sql

---
+++ gs_homework_20171007/gs_an_table.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2017 年 10 月 12 日 16:14
+-- サーバのバージョン： 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE IF NOT EXISTS `gs_an_table` (
+`id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `naiyou` text COLLATE utf8_unicode_ci,
+  `indate` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `email`, `naiyou`, `indate`) VALUES
+(3, 'test100', 'test1', 'test1', '2017-09-30 16:34:20'),
+(5, 'test3', 'test3', 'test3', '2017-09-30 16:34:38'),
+(6, 'BBB', 'BBB', 'OOO', '2017-10-03 19:39:53'),
+(7, 'ooo', 'ppp', 'kkkzzz', '2017-10-04 21:52:02'),
+(8, '本A', 'hon.com', 'テスト', '2017-10-07 17:38:19');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
